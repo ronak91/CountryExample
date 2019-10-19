75548e37d0b2391229eacc91f00a3ae89eb61781
Ronak Upadhyay <ronak.upadhyay91@gmail.com>
2019-10-19 18:01:01 +0530

    crashlytics and analytics added

--- app/src/main/java/com/country/app/view/MainActivity.kt
+++ app/src/main/java/com/country/app/view/MainActivity.kt
@@ -3,12 +3,23 @@ package com.country.app.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.country.app.R
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "App OPENED")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "APP LOG")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
 
     }
 }
